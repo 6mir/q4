a3b922a0f13ba23d0af31eda9f6cc00596adb9ae
Amir <m00h4mm4dweb@gmail.com>
2024-11-06 09:18:11 -0800

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -3,7 +3,7 @@ const audio = new Audio();
 let isPlaying = false; // وضعیت پخش آهنگ
 let progressAnimation; // متغیر برای ذخیره انیمیشن نوار پیشرفت
 
-const StopSvgMain = "/assets/svg/pause.svg"; // آیکون توقف
+const StopSvgMain = "https://raw.githubusercontent.com/6mir/q4/refs/heads/main/assets/svg/pause.svg"; // آیکون توقف
 const PlaySvgMain = "/assets/svg/play.svg"; // آیکون پخش
 const PlayBtn = ".play-btn"; // انتخاب دکمه‌های پخش
 
